e13362ddb6f0879663707e6528138d8026d3ddd9
Nathan VTG <nleal@vertigo.com.br>
2022-07-27 14:38:48 -0300

    feat(gitlab) create validators and update formula

--- vkpr/gitlab/parameter/add/src/unix/formula/formula.sh
+++ vkpr/gitlab/parameter/add/src/unix/formula/formula.sh
@@ -3,8 +3,19 @@
 runFormula() {
   PROJECT_ID=$(rawUrlEncode "${PROJECT_NAME}")
   notice "Creating new parameter ${PARAMETER_NAME}"
+  setCredentials
+  validateInputs
   
   ## seting '*' as default value for PARAMETER_SCOPE
   createOrUpdateVariable "$PROJECT_ID" "$PARAMETER_NAME" "$PARAMETER_VALUE" "$PARAMETER_MASKED" "${PARAMETER_SCOPE:-\*}" "$GITLAB_TOKEN"
 }
- 
+
+setCredentials() {
+  GITLAB_USERNAME=$($VKPR_JQ -r .credential.gitlab.username "$VKPR_CREDENTIAL"/gitlab)
+  GITLAB_TOKEN=$($VKPR_JQ -r .credential.gitlab.token "$VKPR_CREDENTIAL"/gitlab)
+}
+
+validateInputs(){
+  validateGitlabUsername "$GITLAB_USERNAME"
+  validateGitlabToken "$GITLAB_TOKEN"
+}
